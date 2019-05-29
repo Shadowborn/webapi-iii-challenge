5d0e8304513ab91cc7a2863f266854842285f8f1
Shadowborn <james.starks023@gmail.com>
2019-05-29 17:22:37 -0600

    Logger added

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-const express = 'express';
+const express = require('express');
+
+const userRouter = require('./users/userRouter.js');
 
 const server = express();
+
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+
+server.use('/users', userRouter);
+
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`${new Date().toISOString()} ${req.method} to ${req.url} ${req.get(
+    'Origin'
+    )}`
+  );
 };
+
+server.use(logger);
 
 module.exports = server;
