8a4d0563dde2123a9258689be3a83836d2c318b9
Shadowborn <james.starks023@gmail.com>
2019-05-29 22:32:21 -0600

    Delete route functionality working

--- users/userRouter.js
+++ users/userRouter.js
@@ -31,7 +31,7 @@ router.get('/:id', async (req, res) => {
         if (users) {
             res.status(200).json(users);
           } else {
-            res.status(404).json({ message: 'Hub not found' });
+            res.status(404).json({ message: 'User not found' });
           }
     } catch (error) {
         console.log(error);
@@ -60,8 +60,19 @@ router.get('/:id/posts', async (req, res) => {
     }
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = await Users.remove(req.params.id);
+        res.status(200).json({
+            url: `/users/${req.params.id}`,
+            operation: `DELETE for hobbit with id ${req.params.id}`
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error, cannot delete'
+        })
+    }
 });
 
 router.put('/:id', (req, res) => {
